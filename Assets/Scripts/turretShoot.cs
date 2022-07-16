40d5f227023835e60133a0ce5b67c4c87791e047
Karpiaisia <71491157+Karpiaisia@users.noreply.github.com>
2022-07-16 18:43:20 +0300

    Basic turret shoot mechanics
    Turret aims at the nearest enemy with tag "Enemy".
    
    Turret shoots projectiles every 0.5 seconds

---
+++ Assets/Scripts/turretShoot.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class turretShoot : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public float launchVelocity = 50f;
+    public bool roundActive = false;
+    public float fireRate = 0.5F;
+    private float nextFire = 0.0F;
+    public Vector3 enemyPos;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+        if (roundActive == true && Time.time > nextFire)
+        {
+
+
+            enemyPos = FindClosestEnemy().transform.position;
+
+            GameObject ball = Instantiate(projectile, transform.position, transform.rotation);
+            ball.GetComponent<Rigidbody>().AddForce(enemyPos * launchVelocity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+
+    //Transform GetClosestEnemy(Transform[] enemies)
+    //{
+    //    Transform bestTarget = null;
+    //    float closestDistanceSqr = Mathf.Infinity;
+    //    Vector3 currentPos = transform.position;
+    //    foreach (transform potentialTarget in enemies)
+    //    {
+    //        Vector3 directionToTarget = potentialTarget.position - currentPosition;
+    //        float dSqrToTarget = directionToTarget.sqrMagnitude;
+    //        if(dSqrToTarget < closestDistanceSqr)
+    //        {
+    //            closestDistanceSqr = dSqrToTarget;
+    //            bestTarget = potentialTarget;
+    //        }
+    //    }
+    //    return bestTarget;
+
+    //}
+
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemies.Length == 0)
+        {
+            return null;
+        }
+        GameObject closest;
+        if (enemies.Length == 1)
+        {
+            closest = enemies[0];
+            return closest;
+        }
+
+        closest = enemies.OrderBy(go => (transform.position - go.transform.position).sqrMagnitude).First();
+        Debug.Log("targetting" + closest.transform.position);
+        return closest;
+    }
+}
